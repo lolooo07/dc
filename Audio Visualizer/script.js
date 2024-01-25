476e4388c978bf041cae5a993093d0983e143e18
lolooo07 <lolooo07@users.noreply.github.com>
2024-01-25 00:00:00 +0000

    fix typo in variable names

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -163,4 +163,5 @@ playPauseBtn.addEventListener('click', () => {
         playPauseBtn.textContent = "Pause";
         isPlaying = true;
     } else {
-        audioSource.pause();
+        audioSource.pause();
+        playPauseBtn.textContent = "Play";
