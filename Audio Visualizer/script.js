caae7343d5a7d356f5595ad95274ccfd487ca09f
lolooo07 <lolooo07@users.noreply.github.com>
2024-01-26 00:00:00 +0000

    fix: small bug fixes and adjustments

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -165,4 +165,5 @@ playPauseBtn.addEventListener('click', () => {
     } else {
         audioSource.pause();
         playPauseBtn.textContent = "Play";
-        isPlaying = false;
+        isPlaying = false;
+    }
