939028a157676d2bcf89e88e98813a09228c322d
lolooo07 <lolooo07@users.noreply.github.com>
2024-01-30 00:00:00 +0000

    update configuration settings

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -183,4 +183,5 @@ function drawVisualizer(bufferLength, dataArray) {
     
     // Get the frequency data
     analyser.getByteFrequencyData(dataArray);
-
+
+    const barWidth = (visualizerCanvas.width / bufferLength) * 2;
